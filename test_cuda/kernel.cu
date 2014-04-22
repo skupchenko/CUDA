4168eaec8aa80e34be6d9789e727c0846b7c36b0
skupchenko <skupchenko@i.ua>
2014-04-22 15:31:16 +0300

    New
    New

---
+++ test_cuda/kernel.cu
@@ -0,0 +1,24 @@
+__global__ void kernel()
+{
+	long long i = 0;
+	while(i < 10000000000000000000)
+	{
+		++i;
+	}
+}
+
+int main(void)
+{
+
+	kernel<<<1, 16>>>();
+
+	//Хендл event'а
+	cudaEvent_t syncEvent;
+
+	cudaEventCreate(& syncEvent);		//Создаем event
+	cudaEventRecord(syncEvent, 0);		//Записываем event
+	cudaEventSynchronize(syncEvent);	//Синхронизируем event
+
+	return 0;
+
+}
